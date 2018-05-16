6f22b13f6af66a314c6e79bdc761c2bb815130b5
Laura Crisan <lauracrisan@cr-macx.local>
2018-05-16 18:54:24 +0200

    A progress bar -object oriented JS

---
+++ progress_bar-oop/js/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function ProgressBar(value,max) {
+  this.value = value;
+  this.max = max;
+  this.increment = function() {
+    if(this.value < this.max) {
+      this.value += 1;
+    }
+  }
+
+  this.decrement = function() {
+    if(this.value > 0) {
+      this.value -= 1;
+    }
+  }
+}
+
+let progressBar = new ProgressBar(5,30);
+
+function update_html(bar) {
+  $('#counter').html(bar.value);
+  $('#maximum').html(bar.max);
+  $('#knob').css('width', (bar.value/bar.max) * 100 + '%');
+}
+
+$(document).ready(function() {
+  update_html(progressBar);
+
+  $('#plus').click(function() {
+    progressBar.increment();
+    update_html(progressBar);
+  });
+
+  $('#minus').click(function() {
+    progressBar.decrement();
+    update_html(progressBar);
+  });
+});
